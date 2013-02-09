7ca8669b4abf710caefa2f26a84eb6157dee55df
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-02-09 18:28:40 -0500

    Rename variables to ensure installer does not conflict with service config

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 
 set -u
 
-JENKINS_USER=${JENKINS_USER:-"jenkins"}
-JENKINS_HOME=${JENKINS_HOME:-"/var/lib/${JENKINS_USER}"}
-JENKINS_CONF=${JENKINS_HOME}/Library/Preferences/org.jenkins-ci.slave.jnlp.conf
+SERVICE_USER=${SERVICE_USER:-"jenkins"}
+SERVICE_HOME=${SERVICE_HOME:-"/var/lib/${SERVICE_USER}"}
+SERVICE_CONF=${SERVICE_HOME}/Library/Preferences/org.jenkins-ci.slave.jnlp.conf
 MASTER_NAME=""							# set default to jenkins later
 MASTER_USER=""							# set default to `whoami` later
 MASTER=""
@@ -23,66 +23,66 @@ INSTALL_TMP=`mktemp -d -q -t org.jenkins-ci.slave.jnlp`
 
 function create_user() {
 	# see if user exists
-	if dscl /Local/Default list /Users | grep -q ${JENKINS_USER} ; then
-		echo "Using pre-existing service account ${JENKINS_USER}"
-		JENKINS_HOME=`dscl /Local/Default read /Users/Jenkins NFSHomeDirectory | awk '{print $2}'`
+	if dscl /Local/Default list /Users | grep -q ${SERVICE_USER} ; then
+		echo "Using pre-existing service account ${SERVICE_USER}"
+		SERVICE_HOME=`dscl /Local/Default read /Users/Jenkins NFSHomeDirectory | awk '{print $2}'`
 		return 0
 	fi
-	echo "Creating service account ${JENKINS_USER}..."
+	echo "Creating service account ${SERVICE_USER}..."
 	# create jenkins group
 	NEXT_GID=$((`dscl /Local/Default list /Groups gid | awk '{ print $2 }' | sort -n | grep -v ^[5-9] | tail -n1` + 1))
-	sudo dscl /Local/Default create /Groups/${JENKINS_USER}
-	sudo dscl /Local/Default create /Groups/${JENKINS_USER} PrimaryGroupID $NEXT_GID
-	sudo dscl /Local/Default create /Groups/${JENKINS_USER} Password \*
-	sudo dscl /Local/Default create /Groups/${JENKINS_USER} RealName 'Jenkins Node Service'
+	sudo dscl /Local/Default create /Groups/${SERVICE_USER}
+	sudo dscl /Local/Default create /Groups/${SERVICE_USER} PrimaryGroupID $NEXT_GID
+	sudo dscl /Local/Default create /Groups/${SERVICE_USER} Password \*
+	sudo dscl /Local/Default create /Groups/${SERVICE_USER} RealName 'Jenkins Node Service'
 	# create jenkins user
 	NEXT_UID=$((`dscl /Local/Default list /Users uid | awk '{ print $2 }' | sort -n | grep -v ^[5-9] | tail -n1` + 1))
-	sudo dscl /Local/Default create /Users/${JENKINS_USER}
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} UniqueID $NEXT_UID
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} PrimaryGroupID $NEXT_GID
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} UserShell /bin/bash
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} NFSHomeDirectory ${JENKINS_HOME}
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} Password \*
-	sudo dscl /Local/Default create /Users/${JENKINS_USER} RealName 'Jenkins Node Service'
-	sudo dseditgroup -o edit -a ${JENKINS_USER} -t user ${JENKINS_USER}
+	sudo dscl /Local/Default create /Users/${SERVICE_USER}
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} UniqueID $NEXT_UID
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} PrimaryGroupID $NEXT_GID
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} UserShell /bin/bash
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} NFSHomeDirectory ${SERVICE_HOME}
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} Password \*
+	sudo dscl /Local/Default create /Users/${SERVICE_USER} RealName 'Jenkins Node Service'
+	sudo dseditgroup -o edit -a ${SERVICE_USER} -t user ${SERVICE_USER}
 }
 
 function install_files() {
 	# create the jenkins home dir
-	if [ ! -d ${JENKINS_HOME} ] ; then
-		sudo mkdir ${JENKINS_HOME}
+	if [ ! -d ${SERVICE_HOME} ] ; then
+		sudo mkdir ${SERVICE_HOME}
 	fi
 	# download the LaunchDaemon
-	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/org.jenkins-ci.slave.jnlp.plist -o ${JENKINS_HOME}/org.jenkins-ci.slave.jnlp.plist
-	sudo sed -i '' "s#\${JENKINS_HOME}#${JENKINS_HOME}#g" ${JENKINS_HOME}/org.jenkins-ci.slave.jnlp.plist
-	sudo sed -i '' "s#\${JENKINS_USER}#${JENKINS_USER}#g" ${JENKINS_HOME}/org.jenkins-ci.slave.jnlp.plist
+	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/org.jenkins-ci.slave.jnlp.plist -o ${SERVICE_HOME}/org.jenkins-ci.slave.jnlp.plist
+	sudo sed -i '' "s#\${JENKINS_HOME}#${SERVICE_HOME}#g" ${SERVICE_HOME}/org.jenkins-ci.slave.jnlp.plist
+	sudo sed -i '' "s#\${JENKINS_USER}#${SERVICE_USER}#g" ${SERVICE_HOME}/org.jenkins-ci.slave.jnlp.plist
 	sudo rm -f /Library/LaunchDaemons/org.jenkins-ci.slave.jnlp.plist
-	sudo ln -s ${JENKINS_HOME}/org.jenkins-ci.slave.jnlp.plist /Library/LaunchDaemons/org.jenkins-ci.slave.jnlp.plist
+	sudo ln -s ${SERVICE_HOME}/org.jenkins-ci.slave.jnlp.plist /Library/LaunchDaemons/org.jenkins-ci.slave.jnlp.plist
 	# download the jenkins JNLP slave script
-	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/slave.jnlp.sh -o ${JENKINS_HOME}/slave.jnlp.sh
-	sudo chmod 755 ${JENKINS_HOME}/slave.jnlp.sh
+	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/slave.jnlp.sh -o ${SERVICE_HOME}/slave.jnlp.sh
+	sudo chmod 755 ${SERVICE_HOME}/slave.jnlp.sh
 	# download the jenkins JNLP security helper script
-	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/security.sh -o ${JENKINS_HOME}/security.sh
-	sudo chmod 755 ${JENKINS_HOME}/security.sh
+	sudo curl --silent --url https://raw.github.com/rhwood/jenkins-slave-osx/master/security.sh -o ${SERVICE_HOME}/security.sh
+	sudo chmod 755 ${SERVICE_HOME}/security.sh
 	# jenkins should own jenkin's home directory and all its contents
-	sudo chown -R ${JENKINS_USER}:wheel ${JENKINS_HOME}
+	sudo chown -R ${SERVICE_USER}:${SERVICE_USER} ${SERVICE_HOME}
 	# create a logging space
-	if [ ! -d /var/log/${JENKINS_USER} ] ; then
-		sudo mkdir /var/log/${JENIKINS_USER}
-		sudo chown ${JENKINS_USER}:wheel /var/log/${JENKINS_USER}
+	if [ ! -d /var/log/${SERVICE_USER} ] ; then
+		sudo mkdir /var/log/${SERVICE_USER}
+		sudo chown ${SERVICE_USER}:wheel /var/log/${SERVICE_USER}
 	fi
 }
 
 function process_args {
-	if [ -f ${JENKINS_CONF} ]; then
-		sudo chmod 666 ${JENKINS_CONF}
-		source ${JENKINS_CONF}
-		sudo chmod 400 ${JENKINS_CONF}
+	if [ -f ${SERVICE_CONF} ]; then
+		sudo chmod 666 ${SERVICE_CONF}
+		source ${SERVICE_CONF}
+		sudo chmod 400 ${SERVICE_CONF}
 	fi
-	if [ -f ${JENKINS_HOME}/Library/.keychain_pass ]; then
-		sudo chmod 666 ${JENKINS_HOME}/Library/.keychain_pass
-		source ${JENKINS_HOME}/Library/.keychain_pass
-		sudo chmod 400 ${JENKINS_HOME}/Library/.keychain_pass
+	if [ -f ${SERVICE_HOME}/Library/.keychain_pass ]; then
+		sudo chmod 666 ${SERVICE_HOME}/Library/.keychain_pass
+		source ${SERVICE_HOME}/Library/.keychain_pass
+		sudo chmod 400 ${SERVICE_HOME}/Library/.keychain_pass
 	fi
 	while [ $# -gt 0 ]; do
 		case $1 in
@@ -120,7 +120,7 @@ function configure_daemon {
 		SLAVE_NODE=${RESPONSE:-$SLAVE_NODE}
 	fi
 	if [ -z $MASTER_USER ]; then
-		[ "${JENKINS_USER}" != "jenkins" ] && MASTER_USER=${JENKINS_USER} || MASTER_USER=`whoami`
+		[ "${SERVICE_USER}" != "jenkins" ] && MASTER_USER=${SERVICE_USER} || MASTER_USER=`whoami`
 		echo
 		read -p "Account that ${SLAVE_NODE} connects to ${MASTER_NAME} as [${MASTER_USER}]: " RESPONSE
 		MASTER_USER=${RESPONSE:-$MASTER_USER}
@@ -136,7 +136,9 @@ function configure_daemon {
 	OSX_KEYCHAIN_PASS=${OSX_KEYCHAIN_PASS:-`env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 20`}
 	KEYSTORE_PASS=`env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 20`
 	if [ "$PROTOCOL" == "https" ]; then
-		if sudo -i -u ${JENKINS_USER} java -jar ${INSTALL_TMP}/slave.jar -jnlpUrl ${MASTER}/computer/${SLAVE_NODE}/slave-agent.jnlp -jnlpCredentials ${MASTER_USER}:${SLAVE_TOKEN} 2>&1 | grep -q '\-noCertificateCheck' ; then
+		sudo -i -u ${SERVICE_USER} curl --location --url ${MASTER}/jnlpJars/slave.jar --silent --output ${SERVICE_HOME}/slave.jar
+		if sudo -i -u ${SERVICE_USER} java -jar ${SERVICE_HOME}/slave.jar -jnlpUrl ${MASTER}/computer/${SLAVE_NODE}/slave-agent.jnlp -jnlpCredentials ${MASTER_USER}:${SLAVE_TOKEN} 2>&1 | grep -q '\-noCertificateCheck' ; then
+			echo "need certs"
 			if [[ -z $MASTER_CERT && -z $MASTER_CA ]]; then
 				echo
 				echo "The certificate for ${MASTER_NAME} is not trusted by java"
@@ -155,36 +157,36 @@ function configure_daemon {
 					echo "Unable to read ${MASTER_CERT}"
 					read -p "Path to certificate: " MASTER_CERT
 				done
-				sudo -i -u ${JENKINS_USER} keytool -import -alias jenkins-cert -file ${MASTER_CERT} -keystore ${JENKINS_HOME}/.keystore -storepass ${KEYSTORE_PASS}
+				sudo -i -u ${SERVICE_USER} keytool -import -alias jenkins-cert -file ${MASTER_CERT} -keystore ${SERVICE_HOME}/.keystore -storepass ${KEYSTORE_PASS}
 			fi
 			if [ ! -z $MASTER_CA ]; then
 				while [ ! -f $MASTER_CA ]; do
 					echo "Unable to read ${MASTER_CA}"
 					read -p "Path to certificate: " MASTER_CA
 				done
-				sudo -i -u ${JENKINS_USER} keytool -import -alias jenkins-ca -file ${MASTER_CA} -keystore ${JENKINS_HOME}/.keystore -storepass ${KEYSTORE_PASS}
+				sudo -i -u ${SERVICE_USER} keytool -import -alias jenkins-ca -file ${MASTER_CA} -keystore ${SERVICE_HOME}/.keystore -storepass ${KEYSTORE_PASS}
 			fi
-			sudo -i -u ${JENKINS_USER} ${JENKINS_HOME}/security.sh --keychain-pass=${OSX_KEYCHAIN_PASS} --keychain=${OSX_KEYCHAIN} --password=${KEYSTORE_PASS} --account=jenkins --service=java_truststore
+			sudo -i -u ${SERVICE_USER} ${SERVICE_HOME}/security.sh --keychain-pass=${OSX_KEYCHAIN_PASS} --keychain=${OSX_KEYCHAIN} --password=${KEYSTORE_PASS} --account=jenkins --service=java_truststore
 		fi
 	fi
 	
 }
 
 function write_config {
-	sudo mkdir -p `dirname ${JENKINS_CONF}`
-	sudo chmod 777 `dirname ${JENKINS_CONF}`
-	if [ -f ${JENKINS_CONF} ]; then
-		sudo chmod 666 ${JENKINS_CONF}
+	sudo mkdir -p `dirname ${SERVICE_CONF}`
+	sudo chmod 777 `dirname ${SERVICE_CONF}`
+	if [ -f ${SERVICE_CONF} ]; then
+		sudo chmod 666 ${SERVICE_CONF}
 	fi
-	:> ${JENKINS_CONF}
-	echo "JENKINS_SLAVE=${SLAVE_NODE}" >> ${JENKINS_CONF}
-	echo "JENKINS_MASTER=${MASTER}" >> ${JENKINS_CONF}
-	echo "JENKINS_PORT=${MASTER_PORT}" >> ${JENKINS_CONF}
-	echo "JENKINS_USER=${MASTER_USER}" >> ${JENKINS_CONF}
-	echo "JAVA_ARGS=${JAVA_ARGS}" >> ${JENKINS_CONF}
-	sudo chmod 755 `dirname ${JENKINS_CONF}`
-	sudo chmod 644 ${JENKINS_CONF}
-	sudo chown -R ${JENKINS_USER}:${JENKINS_USER} ${JENKINS_HOME}
+	:> ${SERVICE_CONF}
+	echo "JENKINS_SLAVE=${SLAVE_NODE}" >> ${SERVICE_CONF}
+	echo "JENKINS_MASTER=${MASTER}" >> ${SERVICE_CONF}
+	echo "JENKINS_PORT=${MASTER_PORT}" >> ${SERVICE_CONF}
+	echo "JENKINS_USER=${MASTER_USER}" >> ${SERVICE_CONF}
+	echo "JAVA_ARGS=${JAVA_ARGS}" >> ${SERVICE_CONF}
+	sudo chmod 755 `dirname ${SERVICE_CONF}`
+	sudo chmod 644 ${SERVICE_CONF}
+	sudo chown -R ${SERVICE_USER}:${SERVICE_USER} ${SERVICE_HOME}
 }
 
 function cleanup {
